341cc3fb9b8a102d09732188dd682e226ecfbc86
veenacc <veena.chenicheri@gmail.com>
2024-05-23 11:47:00 -0400

    completed js var1

---
+++ try.js
@@ -0,0 +1,25 @@
+// console.log("hello world")
+
+// let x=10
+// // console.log(x+10)
+// console.log(x*10)
+
+// let word= "hello"
+// console.log(word.toUpperCase())
+// console.log(word.split("").reverse().join(""))
+
+
+// let x=10
+// var y=20
+// // console.log(x**y)
+// console.log(x*y)
+
+let x="HELLO"
+// let y ="world"
+// // console.log(x.toUpperCase() + y.toUpperCase())
+// console.log(x.length)
+console.log(x.toLowerCase())
+
+// let x =3.914
+// console.log(Math.trunc(x))
+// console.log(parseInt(x))
